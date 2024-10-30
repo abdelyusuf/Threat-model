913b8d6b283953bfe16749627ac6f6b46ac36b06
Abdel Yusuf <abdul-mukhtar@live.co.uk>
2024-10-30 23:43:02 +0000

    variables

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_version = ">= 1.3.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.67.0"
+      source = "hashicorp/aws"
+      version = "5.73.0"
     }
   }
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  # Configuration options
 }
